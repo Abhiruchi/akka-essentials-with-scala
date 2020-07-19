f9a3db6c8966f7f0a8c6edf3c0d8d7e52b4d44b4
abhirgup <abhirgup@adobe.com>
2020-07-19 18:54:05 +0530

    scala recap

---
+++ src/main/scala/scalarecap/scalarecap.scala
@@ -0,0 +1,35 @@
+package scalarecap
+
+object scalarecap extends App {
+  // val and var
+
+  // expressions
+
+  // code block
+
+  // unit type
+
+  // unit types and functions
+
+  // recursion
+
+  // OOP and traits, method notations, anonymous classes
+
+  // generics and companion objects
+
+  // case classes
+
+  // exceptions
+
+  // functional programming
+
+  // for comprehensions
+
+  // Seq, Array, List, Map, Tuple, Vector, Sets
+
+  // collections - option and try
+
+  // pattern matching
+
+  // s interpolated strings
+}
